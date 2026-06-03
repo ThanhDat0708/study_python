b9053ed21352efc26ca79a3030a35d3101c4fd1e
DESKTOP-0A3K9BM\DELL <lethannhdat07@gmail.com>
2026-06-03 20:57:51 +0700

    first commit

---
+++ check_prime_number.py
@@ -0,0 +1,18 @@
+# viet chuong trinh in ra cac so nguyen to tu 1 den n
+import math
+def check_snt(n: int ) -> bool:
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    for i in range(2,int(math.sqrt(n))+1):
+        if  n % i == 0:
+            return False
+        
+    return True 
+#end function
+n = int(input("Nhap vao 1 so: "))
+print(f"so nguyen to tu 1 den {n} la:")
+for i in range(1, n + 1):
+    if check_snt(i) == True:
+        print(f"{i} ")
