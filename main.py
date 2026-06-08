235c3b14ee798aae6f3a0dd125dd2b6776c464ac
DESKTOP-0A3K9BM\DELL <lethannhdat07@gmail.com>
2026-06-08 22:12:40 +0700

    up

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-import os
+
 
 load_dotenv()
 
 client = OpenAI()
+chat_histories = []
 while True:
-    user_input = input("chat gi di (exit=0):")
-    if user_input == '0':
+    user_input = input("chat gi di:")
+    if not user_input:
         break
+
+    chat_histories.append({
+        'role':'user',
+        'content': user_input
+    })
+    
+
     response = client.responses.create(
         model= 'gpt-5.4-mini',
-        input= user_input
+        instructions= 'Chỉ trả kời câu hỏi liên quan toán học và lập trình ',
+        input=chat_histories
 )
     data = response.output_text
-    print("Ai tra loi:",data)
+    print(f"Ai tra loi:",data)
+
+    chat_histories.append({
+        'role': 'assistant',
+        'content' : data
+    })
